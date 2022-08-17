fb446776a960b8c4cb968270b89accb99e687cf5
Artziel Narvaiza <artziel@gmail.com>
2022-08-17 17:23:35 -0500

    update

--- sample/main.go
+++ sample/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	SystemD "github.com/artziel/go-systemd"
 )
 
+func getExecutablePath() (string, error) {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return "", err
+	}
+
+	return dir + "/" + filepath.Base(os.Args[0]), err
+}
+
 func main() {
 
-	exec, _ := SystemD.GetExecutablePath()
+	exec, _ := getExecutablePath()
 	srv := SystemD.NewService(
 		"sample-daemon",
 		SystemD.Parameters{
